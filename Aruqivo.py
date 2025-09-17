49ce901762829343739487bde90d1694d9134a3a
marcosdavim-del <marcosdavim@gmail.com>
2025-09-17 11:46:37 -0300

    aula de c

--- Aruqivo.py
+++ Aruqivo.py
@@ -1,4 +1,11 @@
-name = input("digite seu nome: ")
-idade = int(input("Digite o ano em q vc nasceu: "))
-2077 = int
-print ("em 2077 vc terá "),
+def calcular_idade_em_2077(ano_nascimento):
+    idade_em_2077 = 2077 - ano_nascimento
+    return idade_em_2077
+
+# Solicitar o ano de nascimento ao usuário
+ano_nascimento = int(input("Digite seu ano de nascimento: "))
+
+# Calcular a idade em 2077
+idade = calcular_idade_em_2077(ano_nascimento)
+
+print(f"Sua idade em 2077 será: {idade} anos.")
